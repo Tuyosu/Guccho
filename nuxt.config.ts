7759b82071897ea74e0ecdacd07dc8ded395042d
nyoemii <hdxdlolMC@gmail.com>
2025-11-22 05:11:13 +0000

    Fix^2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -50,6 +50,9 @@ export default defineNuxtConfig({
         },
       },
     },
+    server: {
+      allowedHosts: ['tuyosu.de', 'localhost', '127.0.0.1'],
+    },
   },
 
   typescript: {
@@ -93,7 +96,7 @@ export default defineNuxtConfig({
         {
           rel: 'stylesheet',
           href:
-          'https://fonts.googleapis.com/css2?family=Kodchasan:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&display=swap',
+            'https://fonts.googleapis.com/css2?family=Kodchasan:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&display=swap',
           media: 'print',
           onload: 'this.media = "all"',
         },
